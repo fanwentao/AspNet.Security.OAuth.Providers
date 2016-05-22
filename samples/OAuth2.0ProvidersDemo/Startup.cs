e17d19fd11859966039baae64e3af91662a362ea
a8204848 <a421295298@vip.qq.com>
2016-05-22 14:05:09 +0800

    Sample

---
+++ samples/OAuth2.0ProvidersDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OAuth2._0ProvidersDemo.Startup))]
+namespace OAuth2._0ProvidersDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
